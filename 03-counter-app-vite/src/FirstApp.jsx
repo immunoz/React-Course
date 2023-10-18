fb284f41fe93dbba62d880c01a66c32337fd7000
Isaac Muñoz Cruz <isaac-mc99@hotmail.com>
2023-10-18 22:08:05 +0200

    React section 2: First steps

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,10 @@
+const FirstApp = () => {
+  return (
+    <>
+        <h1>FIRST APP COMPONENT</h1>
+        <span>falskjdflj</span>
+    </>
+  )
+}
+
+export default FirstApp
---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HelloWorldApp = () => {
+  return (
+    <h1>
+      HELLO WORLD :D!
+    </h1>
+  )
+}
+
+export default HelloWorldApp
